3be4250f332eae76491c329d3f35993d99d8bf3c
FReinwald <reinwald.fabian@web.de>
2021-11-20 23:09:18 +0100

    added file for drawing of goblin picture
    Image-Conversion.py applies model to webcam image (with face detection)
    Image-to-txt.py makes image drawable for robot

---
+++ code/Image-Conversion.py
@@ -0,0 +1,82 @@
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import time
+
+import numpy as np
+import cv2
+from PIL import Image
+
+import os
+from options.test_options import TestOptions
+from options.train_options import TrainOptions
+from data import create_dataset
+from models import create_model
+from util import html
+import torch
+import torchvision
+import torchvision.transforms as transforms
+
+from helper_demo import helper
+
+
+def load_model(model_name):
+    opt = helper.setup_options()
+    opt.name = model_name
+    model = create_model(opt)
+    model.isTrain = False
+    model.setup(opt)
+
+    return model
+
+
+def get_webcam(model, mirror=False):
+    cam = cv2.VideoCapture(1)
+    while True:
+        ret_val, img = cam.read()
+        if mirror:
+            img = cv2.flip(img, 1)
+        cv2.imshow('my webcam', img)
+        if cv2.waitKey(1) == 27:
+            break  # esc to quit
+    cv2.destroyAllWindows()
+    return img
+
+
+def crop_face_from_webcam(frame):
+    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+    face = face_cascade.detectMultiScale(cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY), 1.1, 4)
+    for (x, y, w, h) in face:
+        padding = int(0.5 * w)
+        face = frame[y-padding:y+h+padding, x-padding:x+w+padding]
+    return face
+
+
+def face_to_goblin(frame, model):
+    cvframe = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    transform = transforms.Compose([torchvision.transforms.functional.hflip,
+                                    transforms.CenterCrop(256),
+                                    transforms.ToTensor(),
+                                    transforms.Normalize((0.5, 0.5, 0.5),
+                                                         (0.5, 0.5, 0.5))])
+    pil_img = Image.fromarray(cvframe)
+    img = transform(pil_img)
+    img = img.view(1, 3, 256, 256)
+    img_A = helper.to_image(img)
+    img_B = model.gen_B(img, 1)
+    img_B = helper.to_image(img_B)
+    img_AB = helper.concatenate([img_A, img_B])
+
+    plt.axis('off')
+    plt.title('Generated goblin face')
+    plt.imshow(img_AB)
+    plt.show()
+
+
+def main():
+    model = load_model("goblin")
+    img = get_webcam(model, mirror=True)
+    face = crop_face_from_webcam(img)
+    face_to_goblin(face, model)
+
+if __name__ == '__main__':
+    main()
---
+++ code/draw-to-robot/Robot-Simulation.py
@@ -0,0 +1,47 @@
+#Robot-Simulation
+#This file converts a txt file of coordinates to a visual representation of the robot movement
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+#Robot specific drawing area
+xmax = 170
+ymax = 145
+zmax = 30
+drawing_plane = 10
+
+#set path to file
+path_to_file_folder = 'C:/Users/Fabian/OneDrive - stud.hs-heilbronn.de/HHN/WS2122/Semesterarbeit/repo Ki kunst/'
+txt_file_name = 'robotmove.txt'
+path_to_file = path_to_file_folder+txt_file_name
+
+#read txt file with path coordinates
+coords = np.loadtxt(path_to_file)
+
+#slice txt in x,y and z
+x = coords[0::3]
+y = coords[1::3]
+z = coords[2::3]
+
+#direction of vectors
+x_dir = x[1:]
+x_dir = np.append(x_dir, 0)-x
+x_dir[-1] = 0
+y_dir = y[1:]
+y_dir = np.append(y_dir, 0)-y
+y_dir[-1] = 0
+z_dir = z[1:]
+z_dir = np.append(z_dir, 0)-z
+z_dir[-1] = 0
+
+#todo: find way to properly color quivers depending if the robot draws or not
+bool_not_drawing = z > drawing_plane
+c = [0, 0, 0]
+
+#drawing of plot
+ax = plt.figure().add_subplot(projection='3d')
+ax.set(xlim=(0, xmax), ylim=(0, ymax), zlim=(0, zmax))
+ax.quiver(x, y, z, x_dir, y_dir, z_dir, colors=c, normalize=True, arrow_length_ratio=0.05)
+# Disable
+# ax.quiver(x, y, z, x_dir, y_dir, z_dir, colors=c, normalize=False, arrow_length_ratio=0.05)
+plt.show()
